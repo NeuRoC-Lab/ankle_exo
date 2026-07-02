1598dff2d8aa2deab492909f56610bd832afcb50
siqii147 <siqiyang2005@gmail.com>
2026-07-02 15:11:27 -0400

    Finish implementing getting velocity from encoder

--- software/src/plotTwoEncoders.py
+++ software/src/plotTwoEncoders.py
@@ -1,6 +1,7 @@
 """
 This code is used to the plot the relative angle of both encoders in degrees relative to their respective neutral position
 The neutral position is recorded at the start of the test
+The code is now updated to also plot raw velocity and filtered velocity using EWMA
 
 To run this script, type in terminal of CLion:
 
@@ -52,6 +53,13 @@ time.sleep(1)
 
 left_angle = []
 right_angle = []
+
+left_raw_velocity = []
+right_raw_velocity = []
+
+left_filtered_velocity = []
+right_filtered_velocity = []
+
 time_data = []
 
 start_time = time.perf_counter()
@@ -60,29 +68,53 @@ start_time = time.perf_counter()
 
 plt.ion()
 print("Figure Creating")
-width = 8
-height = 6
+width = 16
+height = 12
 
-fig, axes = plt.subplots(ncols=2, nrows=1, figsize=(width,height))
+fig, axes = plt.subplots(ncols=2, nrows=3, figsize=(width,height))
 ax1 = axes[0, 0]
 ax2 = axes[0, 1]
+ax3 = axes[1, 0]
+ax4 = axes[1, 1]
+ax5 = axes[2, 0]
+ax6 = axes[2, 1]
 
 fig.suptitle("Real Time Encoder Position for Left and Right Ankle Exoskeletons", fontsize=14)
 
 line1, = ax1.plot(time_data, left_angle, linewidth=1.5, color="red")
-line2, = ax2.plot(time_data, right_angle, linewidth=1.5, color="blue")
+line2, = ax2.plot(time_data, right_angle, linewidth=1.5, color="green")
+line3, = ax3.plot(time_data, left_raw_velocity, linewidth=1.5, color="orange")
+line4, = ax4.plot(time_data, right_raw_velocity, linewidth=1.5, color="blue")
+line5, = ax5.plot(time_data, left_filtered_velocity, linewidth=1.5, color="yellow")
+line6, = ax6.plot(time_data, right_filtered_velocity, linewidth=1.5, color="purple")
 
 ax1.set_title("Left Encoder Position Over Time")
 ax2.set_title("Right Encoder Position Over Time")
+ax3.set_title("Left Encoder Raw Velocity Over Time")
+ax4.set_title("Right Encoder Raw Velocity Over Time")
+ax5.set_title("Left Encoder Filtered Velocity Over Time")
+ax6.set_title("Right Encoder Filtered Velocity Over Time")
 
 ax1.set_xlabel("Time (s)")
 ax2.set_xlabel("Time(s)")
+ax3.set_xlabel("Time (s)")
+ax4.set_xlabel("Time (s)")
+ax5.set_xlabel("Time (s)")
+ax6.set_xlabel("Time (s)")
 
 ax1.set_ylabel("Left encoder angle (deg)")
 ax2.set_ylabel("Right encoder angle (deg)")
+ax3.set_ylabel("Left encoder raw velocity (deg/s)")
+ax4.set_ylabel("Right encoder raw velocity (deg/s)")
+ax5.set_ylabel("Left encoder filtered velocity (deg/s)")
+ax6.set_ylabel("Right encoder filtered velocity (deg/s)")
 
 ax1.grid(True)
 ax2.grid(True)
+ax3.grid(True)
+ax4.grid(True)
+ax5.grid(True)
+ax6.grid(True)
 
 fig.canvas.mp1_connect("close_event", close_plot)
 
@@ -90,7 +122,7 @@ plt.show(block=False)
 
 # Start plotting encoder positions
 ser.flush()
-print("Left and right encoder position plotting starts now")
+print("Left and right encoder position and velocity plotting starts now")
 
 try:
 
@@ -111,12 +143,20 @@ try:
         arr = raw.split
 
         try:
-            left_index = arr.index("Left") + 4
-            right_index = arr.index("Right") + 4
+            left_pos_index = 3
+            right_pos_index = 7
+            left_raw_index = 11
+            right_raw_index = 15
+            left_filtered_index = 19
+            right_filtered_index = 23
 
             # y-axis data
-            left_angle.append(float(arr[left_index]))
-            right_angle.append(float(arr[right_index]))
+            left_angle.append(float(arr[left_pos_index]))
+            right_angle.append(float(arr[right_pos_index]))
+            left_raw_velocity.append(float(arr[left_raw_index]))
+            right_raw_velocity.append(float(arr[right_raw_index]))
+            left_filtered_velocity.append(float(arr[left_filtered_index]))
+            right_filtered_velocity.append(float(arr[right_filtered_index]))
 
         except Exception as e:
             print("Could not read line:", e)
@@ -130,10 +170,18 @@ try:
         while time_data and (current_time - time_data[0]) > time_window:
             left_angle.pop(0)
             right_angle.pop(0)
+            left_raw_velocity.pop(0)
+            right_raw_velocity.pop(0)
+            left_filtered_velocity.pop(0)
+            right_filtered_velocity.pop(0)
 
         # update plots in real line
         line1.set_data(time_data, left_angle)
         line2.set_data(time_data, right_angle)
+        line3.set_data(time_data, left_raw_velocity)
+        line4.set_data(time_data, right_raw_velocity)
+        line5.set_data(time_data, left_filtered_velocity)
+        line6.set_data(time_data, right_filtered_velocity)
 
         ax1.set_xlim(max(0, current_time - time_window), current_time)
         ax1.relim()
@@ -142,6 +190,22 @@ try:
         ax2.set_xlim(max(0, current_time - time_window), current_time)
         ax2.relim()
         ax2.autoscale_view(scalex=False, scaley=True)
+
+        ax3.set_xlim(max(0, current_time - time_window), current_time)
+        ax3.relim()
+        ax3.autoscale_view(scalex=False, scaley=True)
+
+        ax4.set_xlim(max(0, current_time - time_window), current_time)
+        ax4.relim()
+        ax4.autoscale_view(scalex=False, scaley=True)
+
+        ax5.set_xlim(max(0, current_time - time_window), current_time)
+        ax5.relim()
+        ax5.autoscale_view(scalex=False, scaley=True)
+
+        ax6.set_xlim(max(0, current_time - time_window), current_time)
+        ax6.relim()
+        ax6.autoscale_view(scalex=False, scaley=True)
 
 
 except KeyboardInterrupt:
